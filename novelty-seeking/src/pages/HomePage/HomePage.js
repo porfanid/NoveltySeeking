9bc4b8bb6da9b0ff43c470a8019c4befb9b02cab
Pavlos Orfanidis <pavlos@orfanidis.net.gr>
2023-12-13 14:28:51 +0200

    Added link to the first page

--- novelty-seeking/src/pages/HomePage/HomePage.js
+++ novelty-seeking/src/pages/HomePage/HomePage.js
@@ -1,4 +1,5 @@
 import Header from "../../GeneralComponents/Header";
+import {NavLink} from "react-router-dom";
 
 function HomePage(){
     return(
@@ -8,7 +9,9 @@ function HomePage(){
             <p>SnapX Photography is a professional website template with 5 different HTML pages for maximum customizations. It is free for commercial usage. This Bootstrap v5.1.3 CSS layout is provided by TemplateMo Free CSS Templates.</p>
             <div className="buttons">
                 <div className="big-border-button">
-                    <a href="contests">Explore SnapX Contest</a>
+                    <NavLink className="active" to={process.env.PUBLIC_URL+"/choice/1"}>
+                        Ξεκινήστε τις ερωτήσεις
+                    </NavLink>
                 </div>
                 <div className="icon-button">
                     <a href="https://youtube.com/templatemo" target="_blank" rel={"noreferrer"}><i className="fa fa-play"></i> Watch Our Video Now</a>
