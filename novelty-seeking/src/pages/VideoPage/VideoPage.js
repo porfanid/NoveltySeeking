9fb1903b845a2f224eb4d01886b912a215518011
Pavlos Orfanidis <pavlos@orfanidis.net.gr>
2023-12-22 15:29:33 +0200

    Added condition to finish wtching the video before being able to move on

--- novelty-seeking/src/pages/VideoPage/VideoPage.js
+++ novelty-seeking/src/pages/VideoPage/VideoPage.js
@@ -1,9 +1,10 @@
 import Header from "../../GeneralComponents/Header";
 import {NavLink, useParams} from "react-router-dom";
-import {useEffect} from "react";
+import {useEffect, useState} from "react";
 
 function VideoPage(props){
     let { index, choice, category, counter } = useParams();
+    const [videoFinished, setVideoFinished] = useState(false);
 
     useEffect(() => {
         props.setCategoryAndCounter(category, counter);
@@ -16,7 +17,7 @@ function VideoPage(props){
             <h2>Welcome</h2>
             <p>Παρακαλώ παρακολουθήστε το παρακάτω video</p>
 
-            <video className={"img-fluid"} autoPlay={true} muted={true}>
+            <video className={"img-fluid"} autoPlay={true} muted={true} onEnded={()=>{setVideoFinished(true)}}>
                 <source src={process.env.PUBLIC_URL+`/assets/videos/${choice}/${category}/${counter}.mp4`} type="video/mp4"/>
                 Your browser does not support the video tag.
             </video>
@@ -24,9 +25,9 @@ function VideoPage(props){
 
             <div className="buttons">
                 <div className="big-border-button">
-                    <NavLink className="active" to={process.env.PUBLIC_URL+"/quiz/"+index+"/choice/"+choice+"/category/"+category+"/counter/"+counter}>
+                    {videoFinished ? (<NavLink className="disabled" to={process.env.PUBLIC_URL+"/quiz/"+index+"/choice/"+choice+"/category/"+category+"/counter/"+counter}>
                         Επόμενη Σελίδα
-                    </NavLink>
+                    </NavLink>):null}
                 </div>
                 <div className="icon-button">
                     <a href="https://youtube.com/templatemo" target="_blank" rel={"noreferrer"}><i className="fa fa-play"></i> Watch Our Video Now</a>
